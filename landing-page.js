d59d919f95551e43355361c2dcdb406d452c7727
Lawrence McDaniel <lpm0073@gmail.com>
2017-08-10 19:39:15 -0500

    fully syled fish bowl affix

--- landing-page.js
+++ landing-page.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     /* affix the navbar after scroll below header w space for fundx header */
-    $(".navbar").affix({offset: {top: $("header").outerHeight(true) - 25} });
+    $(".navbar").affix({offset: {top: $("header").outerHeight(true)} });
 
     /* affix the fish bowls after scroll below header + large spacer */
 
-    $(".fish-bowl").affix({offset: {top: $("header").outerHeight(true) - 25, bottom: 1275} });
+    $(".fish-bowl").affix({offset: {top: $("header").outerHeight(true) + 52, bottom: 1275} });
 
 });
