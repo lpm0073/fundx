6e629a1f6b30bae492d5033a9443310c637aa5ab
Lawrence McDaniel <lpm0073@gmail.com>
2017-08-10 22:04:43 -0500

    formatting adjustment to migrate to wordpress

--- landing-page.js
+++ landing-page.js
@@ -2,8 +2,13 @@ $(document).ready(function(){
     /* affix the navbar after scroll below header w space for fundx header */
     $(".navbar").affix({offset: {top: $("header").outerHeight(true)} });
 
-    /* affix the fish bowls after scroll below header + large spacer */
+    /* affix the fish bowls after scroll below header + large spacer
 
-    $(".fish-bowl").affix({offset: {top: $("header").outerHeight(true) + 52, bottom: 1275} });
+    $(".fish-bowl").affix({offset:
+        {
+          top: $("header").outerHeight(true) + 52 + $(window).height() * .3,
+          bottom: 1275}
+        });
+*/
 
 });
